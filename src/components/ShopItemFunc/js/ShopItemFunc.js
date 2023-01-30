36d97d0937ece135e7f707faa33faac2ea533915
arbprog <bal.fam@yandex.ru>
2023-01-30 11:11:34 +0300

    ShopItemFunc item props add

--- src/components/ShopItemFunc/js/ShopItemFunc.js
+++ src/components/ShopItemFunc/js/ShopItemFunc.js
@@ -1,22 +1,34 @@
+import PropTypes from 'prop-types';
+
 function ShopItemFunc ({item}) {
+    const cost = Math.round((item.price * 100) / 100).toFixed(2);
     return (
         <div class="main-content">
-            <h2>Tiger of Sweden</h2>
-            <h1>Leonard coat</h1>
-            <h3>Minimalistic coat in cotton-blend</h3>
-            <div class="description">
-                Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
-            </div>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div class="description">{item.descriptionFull}</div>
             <div class="highlight-window mobile">
                 <div class="highlight-overlay"></div>
             </div>
             <div class="divider"></div>
             <div class="purchase-info">
-                <div class="price">{item.currency}{item.price}</div>
+                <div class="price">{item.currency}{cost}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
     );
 };
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.shape({
+      brand: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      descriptionFull: PropTypes.string,
+      currency: PropTypes.string,
+      price: PropTypes.number
+    })
+  }
 
 export default ShopItemFunc;
